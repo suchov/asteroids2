7aaae3a88d7f05708b13633e653a234d31d89290
Artem Sychov <artem@sychov.dev>
2025-09-27 14:10:27 +0200

    commit the black scrin

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+# this allows us to use the codr from
+# the opne-source  pygame library
+# through this file
+import pygame
+from constants import *
+
+BLACK = (0, 0, 0)
+
+
 def main():
-    print("Hello from asteroids2!")
+    pygame.init()
+    print("Starting Asteroids!")
+    print('Screen width:', SCREEN_WIDTH)
+    print('Screen height:', SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
